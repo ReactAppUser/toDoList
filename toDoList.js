9119d13db2559c7429d547946dcd53bb93cdb55c
ReactAppUser <Wec07mer@gmail.com>
2023-05-03 20:09:22 +0300

    Add new function for createElement. Add new DOM element. Add listed method for text Input.

--- toDoList.js
+++ toDoList.js
@@ -6,19 +6,40 @@
         {task: 'Buy'},
         ];
 
-    console.log('toDoList', toDoList);
+    // console.log('toDoList', toDoList);
 
     let elForToDoList = document.querySelector('.toDoList');
+    let elBody = document.querySelector('.body');
+
+    let ElementCreater =  function (Dom, element, attribute1, attributeValue1,  attribute2, attributeValue2,className, value) {
+        let DOMEl = Dom;
+        let readyElement = document.createElement(element);
+        readyElement.setAttribute(attribute1, attributeValue1);
+        readyElement.setAttribute(attribute2, attributeValue2);
+        readyElement.classList.add(className);
+        readyElement.innerHTML = value;
+        DOMEl.append(readyElement);
+        return DOMEl
+    };
 
    toDoList.map((element) => {
-        let liEl = document.createElement('li');
-        liEl.innerHTML = element.task;
-        liEl.classList.add('list-group-item');
-        elForToDoList.append(liEl);
+       ElementCreater(document.querySelector('.toDoList'),'li','aria-current' , null, null,'true', 'list-group-item', element.task );
+        // let liEl = document.createElement('li');
+        // liEl.innerHTML = element.task;
+        // liEl.classList.add('list-group-item');
+        // liEl.setAttribute('aria-current', 'true');
+        // elForToDoList.append(liEl);
        }
     );
+    ElementCreater(document.querySelector('.body'),'form', null,null, null,null ,'form', null  );
+    ElementCreater(document.querySelector('.form'),'input', null,null, 'onchange', "alert(this.value)", 'inputForText', null  );
+    ElementCreater(document.querySelector('.form'),'input', 'type','button', 'value', 'submitButton', 'inputButton', 'Submit Button'  );
 
-   let liInput = document.createElement('input');
-    elForToDoList.append(liInput);
+    // let getInput = document.querySelector('.inputForText')
 
-    console.log('elForToDoList', elForToDoList);
+   // let liInput = document.createElement('input');
+   //  elForToDoList.append(liInput);
+
+    // console.log('elForToDoList', getInput);
+
+
