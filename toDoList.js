380a28606a6a89846bcf32ee335520f8983467ff
ReactAppUser <Wec07mer@gmail.com>
2023-05-19 19:28:33 +0300

    Added new botton and added new dynamic className for element

--- toDoList.js
+++ toDoList.js
@@ -1,56 +1,16 @@
+
     const toDoList = [
-        {task: 'hello'},
-        {task: 'What is your name?'},
-        {task: 'How are you?'},
-        {task: 'what is you like?'},
-        {task: 'Buy'},
+        {task: 'Hello', taskClassName: 'Hello'},
+        {task: 'What Is Your Name?', taskClassName: 'WhatIsYourName?'},
+        {task: 'How are you?', taskClassName: 'HowAreYou?'},
+        {task: 'what is you like?', taskClassName: 'WhatIsYouLike?'},
+        {task: 'Buy', taskClassName: 'Buy'},
         ];
 
     let elForToDoList = document.querySelector('.toDoList');
     let elBody = document.querySelector('.body');
 
-    // let formElement = new CreaterForObjectElement(
-    //     '.body',
-    //     'form',
-    //     '',
-    //     '',
-    //     '',
-    //     '',
-    //     'form',
-    //     '',
-    //     );
-    //
-    // let inputElement = new CreaterForObjectElement(
-    //     '.form',
-    //     'input',
-    //     'onchange',
-    //     'keyDownEnter',
-    //     'onchange',
-    //     'pushEvent(this.value, event)',
-    //     'inputForText',
-    //     '',
-    // );
-    //
-    // let inputButtonElement = new CreaterForObjectElement(
-    //     '.form',
-    //     'input',
-    //     'type',
-    //     'button',
-    //     'value',
-    //     'submitButton',
-    //     'inputButton',
-    //     'Submit Button',
-    // );
-    //
-    // let liElement = new CreaterForObjectElement('.toDoList',
-    //     'li',
-    //     'aria-current',
-    //     'true',
-    //     'list-group-item',
-    // );
-
-
-    let formElement = new CreaterForObjectElement(
+    let formElement = new CreatorForObjectElement(
         '.body',
         'form',
         [['null', 'null'], ['null', 'null']],
@@ -58,7 +18,7 @@
         '',
         );
 
-    let inputElement = new CreaterForObjectElement(
+    let inputElement = new CreatorForObjectElement(
         '.form',
         'input',
         [['onchange', 'keyDownEnter'], ['onchange', 'pushEvent(this.value, event)']],
@@ -66,7 +26,7 @@
         '',
     );
 
-    let inputButtonElement = new CreaterForObjectElement(
+    let inputButtonElement = new CreatorForObjectElement(
         '.form',
         'input',
         [['type', 'button'], ['value', 'submitButton']],
@@ -74,92 +34,56 @@
         'Submit Button',
     );
 
-    let liElement = new CreaterForObjectElement(
+    let chckedButtonElement = new CreatorForObjectElement(
+        '.form',
+        'input',
+        [['type', 'button'], ['value', 'CheckedButton']],
+        'null',
+        'Done checking button',
+    );
+
+    let liElement = new CreatorForObjectElement(
         '.toDoList',
         'li',
         [['aria-current', 'true'], ['null', 'null']],
-        'list-group-item',
+        `null`,
         'null',
     );
 
-    elementCreater(formElement);
+    let checkboxElement = new CreatorForObjectElement(
+        '.list-group-item',
+        'input',
+        [['type', 'checkbox'], ['name', 'null'], ['value', 'null']],
+        'checkbox-group-item',
+        'null',
+    );
 
-    elementCreater(inputElement);
+    elementCreator(formElement);
 
-    elementCreater(inputButtonElement);
+    elementCreator(inputElement);
 
+    elementCreator(inputButtonElement);
 
-    //
-    // function CreaterForObjectElement(dom, element, attribute1, attributeValue1, attribute2, attributeValue2, className, value) {
-    //     this.dom = dom;
-    //     this.element = element;
-    //     this.attribute1 = attribute1 ? attribute1 : null;
-    //     this.attributeValue1 = attributeValue1 ? attributeValue1 : null;
-    //     this.attribute2 = attribute2 ? attribute2 : null;
-    //     this.attributeValue2 = attributeValue2 ? attributeValue2 : null;
-    //     this.className = className;
-    //     this.value = value ? value : null;
-    // };
-    //
-    // arr.forEach(function(item, index, array) {
-    //     // ... делать что-то с item
-    // });
+    elementCreator(chckedButtonElement);
 
 
-
-    function CreaterForObjectElement(dom, element, attribute, className, value) {
+    function CreatorForObjectElement(dom, element, attribute, className, value) {
         this.dom = dom;
         this.element = element;
         this.attribute = attribute ? attribute : null;
-        this.className = className;
+        this.className = className ? className : null;;
         this.value = value ? value : null;
     };
 
-    console.log('CreaterForObjectElement', inputElement);
-
-    // this.attribute.forEach(function(attributeItem, index){
-    //     let attributeNumber = attributeItem + index;
-    //     return this.attribute = attributeNumber ? attributeNumber : null;
-    // });
-    // this.attributeValue.forEach(function(attributeValueItem, index){
-    //     let attributeValueNumber = attributeValueItem + index;
-    //     return this.attributeValue = attributeValueNumber ? attributeValueNumber : null;
-    // });
-
-
-    // function elementCreater(objectElement){
-    //     let DOMEl = document.querySelector(objectElement.dom);
-    //     let readyElement = document.createElement(objectElement.element);
-    //     let attributeArray = objectElement.attribute;
-    //     attributeArray.forEach(function(attributeItem){
-    //         readyElement.setAttribute(attributeItem)});
-    //
-    //     readyElement.setAttribute(objectElement.attribute1, objectElement.attributeValue1);
-    //
-    //
-    //     readyElement.setAttribute(objectElement.attribute2, objectElement.attributeValue2);
-    //     readyElement.classList.add(objectElement.className);
-    //     readyElement.innerHTML = objectElement.value;
-    //     DOMEl.append(readyElement);
-    //     return DOMEl
-    // };
-
-
-    function elementCreater(objectElement){
-        // console.log("objectElement", objectElement)
+    function elementCreator(objectElement){
         let DOMEl = document.querySelector(objectElement.dom);
         let readyElement = document.createElement(objectElement.element);
         let attributeArray = objectElement.attribute;
-        // console.log("attributeArray", attributeArray);
             attributeArray.forEach(function(attributesStack){
-                    // console.log('attributesStack', attributesStack);
                     let attribute = attributesStack[0];
-                    // console.log('attribute', attribute);
                     let attributeValue = attributesStack[1];
-                    // console.log('attributeValue', attributeValue);
                     readyElement.setAttribute(attribute, attributeValue);
          });
-        // console.log('class', objectElement.className)
         readyElement.classList.add(objectElement.className);
         readyElement.innerHTML = objectElement.value;
         DOMEl.append(readyElement);
@@ -168,7 +92,12 @@
 
     function pushEvent(task){
         liElement.value = task;
-        elementCreater(liElement);
+        if (liElement.value) {
+            liElement.className = task;
+        } else {
+            liElement.className = 'newFunction'
+        }
+        elementCreator(liElement);
     };
 
     document.addEventListener('keydown',(event, task) => {
@@ -180,10 +109,19 @@
 
 
     toDoList.map((element) => {
-        liElement.value = element.task;
-        elementCreater(liElement);
+        console.log('element', element);
+         liElement.className = element.taskClassName;
+         liElement.value = element.task;
+         elementCreator(liElement);
     }
     );
+
+    // toDoList.map((element) => {
+
+    //     elementCreator(checkboxElement);
+    //     }
+    // );
+
 
 
 
