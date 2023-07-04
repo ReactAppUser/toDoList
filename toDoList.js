348dfea2bc9eb3a1d156fd3a4725551041a53b2d
ReactAppUser <Wec07mer@gmail.com>
2023-07-04 15:29:27 +0300

    Added functionality for deleted element from toDoList

--- toDoList.js
+++ toDoList.js
@@ -7,6 +7,7 @@ const defaultTasks = [
 ];
 
 let idCount = 5;
+let canChecked = true;
 
 
 const todoList = [];
@@ -15,33 +16,45 @@ const doneList = [];
 const changeTaskStatus = (taskElement) => {
     if (taskElement.checked) {
 
-        console.log('taskElement', taskElement.checked);
+        // console.log('taskElement', taskElement.checked);
 
-        console.log('taskElement', taskElement);
+        // console.log('taskElement', taskElement);
         doneList.push(taskElement.parentElement);
+        todoList.splice(todoList.indexOf(taskElement, 0));
         const elem = document.querySelector(`#${taskElement.id}`).parentElement;
         elem.remove();
-        console.log('elem', elem);
+
         // https://stackoverflow.com/questions/55532767/how-to-find-an-element-by-data-selector-in-an-array-of-html-dom-objects-using-ja
-        // todoList.splice(todoList.findIndex(elem), 0);
+
         console.log('doneList', doneList);
         console.log('todoList after', todoList);
         let doneListElement = document.querySelector('#doneList');
         doneList.map(function(task){
-            console.log('doneListElement', doneListElement);
-            console.log('todoList before', todoList);
+            doneListElement.prepend(task);
+            // console.log('doneListElement', doneListElement);
             console.log('task', task);
-            return doneListElement.prepend(task);
-            
+            console.log('todoList.indexOf(task.id)', todoList.indexOf(task));
+
+            console.log('todoList before', todoList);
+            return doneListElement;
+            // if (doneList.length == 0) {
+            //     canChecked = false;
+            // }
+
         });
         // remove from display in old list
         // add to display in new list
     } else {
         todoList.push(taskElement);
+        taskElement.checked = false;
         const elem = document.querySelector(`#${taskElement.id}`).parentElement;
         let toDoListElement = document.querySelector('#toDoList');
         toDoListElement.append(elem);
-        console.log('elem 2', elem)
+
+        // if (canChecked) {
+        //
+        // }
+        // console.log('elem 2', elem)
         // doneList.splice(doneList.findIndex(elem), 0);
         // same shit
     }
@@ -57,14 +70,15 @@ const createTask = (id, text) => {
     const deleteButton = taskElement.lastChild;
     todoList.push(taskElement);
     document.querySelector('#toDoList').append(taskElement);
+
     //
     checkBox.addEventListener('click', (event) => {
-        console.log('CheckedCheckbox', event.target);
+        // console.log('CheckedCheckbox', event.target);
         changeTaskStatus(event.target);
     })
 
     deleteButton.addEventListener('click', (event) => {
-        console.log('deleteButton', event);
+        // console.log('deleteButton', event);
         taskElement.remove();
     })
 }
@@ -79,9 +93,9 @@ let addNewTaskElementToField = () => {
 
     console.log('addButton', addButton);
     addButton.addEventListener('change', (event) => {
-        console.log('event', event.target.value);
+        // console.log('event', event.target.value);
         todoList.push(createTask(`${'task'+ idTaskNumber()}`, event.target.value));
-        console.log('todoList', todoList);
+        // console.log('todoList', todoList);
         event.target.value = '';
     })
 }
