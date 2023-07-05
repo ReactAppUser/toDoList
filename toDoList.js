f19234cb43a320d97557da38b1ef94221fd36109
ReactAppUser <Wec07mer@gmail.com>
2023-07-05 15:20:50 +0300

    Working on the fixing bugs in functionality, when element returning  with  status checking to to-do list

--- toDoList.js
+++ toDoList.js
@@ -1,9 +1,9 @@
 const defaultTasks = [
-    {text: 'Hello', id: 'task1',},
-    {text: 'What Is Your Name?', id: 'task2',},
-    {text: 'How are you?', id: 'task3',},
-    {text: 'What is you like?', id: 'task4',},
-    {text: 'Buy', id: 'task5'},
+    {text: 'Hello', id: 'task1', taskstatus: false, },
+    {text: 'What Is Your Name?', id: 'task2', taskstatus: false, },
+    {text: 'How are you?', id: 'task3', taskstatus: false, },
+    {text: 'What is you like?', id: 'task4', taskstatus: false, },
+    {text: 'Buy', id: 'task5', taskstatus: false, },
 ];
 
 let idCount = 5;
@@ -14,14 +14,19 @@ const todoList = [];
 const doneList = [];
 
 const changeTaskStatus = (taskElement) => {
+
+
+
+
     if (taskElement.checked) {
-
-        // console.log('taskElement', taskElement.checked);
-
         // console.log('taskElement', taskElement);
         doneList.push(taskElement.parentElement);
         todoList.splice(todoList.indexOf(taskElement, 0));
         const elem = document.querySelector(`#${taskElement.id}`).parentElement;
+        console.log('elem after', elem.getAttribute('taskstatus'));
+        elem.setAttribute('taskstatus', 'true');
+        console.log('elem before', elem.getAttribute('taskstatus'));
+        let elementStatus = elem.getAttribute('taskstatus');
         elem.remove();
 
         // https://stackoverflow.com/questions/55532767/how-to-find-an-element-by-data-selector-in-an-array-of-html-dom-objects-using-ja
@@ -34,8 +39,16 @@ const changeTaskStatus = (taskElement) => {
             // console.log('doneListElement', doneListElement);
             console.log('task', task);
             console.log('todoList.indexOf(task.id)', todoList.indexOf(task));
-
             console.log('todoList before', todoList);
+            if (elementStatus) {
+                elem.setAttribute('taskstatus', 'false');
+                console.log('elementStatus', elementStatus);
+            // const elem = document.querySelector(`#${taskElement.id}`).parentElement;
+            // let toDoListElement = document.querySelector('#toDoList');
+            // // setElementStatus = false;
+            // toDoListElement.append(elem);
+            };
+
             return doneListElement;
             // if (doneList.length == 0) {
             //     canChecked = false;
@@ -45,11 +58,12 @@ const changeTaskStatus = (taskElement) => {
         // remove from display in old list
         // add to display in new list
     } else {
+        // const elem = document.querySelector(`#${taskElement.id}`).parentElement;
+        // let toDoListElement = document.querySelector('#toDoList');
+        // toDoListElement.append(elem);
         todoList.push(taskElement);
-        taskElement.checked = false;
-        const elem = document.querySelector(`#${taskElement.id}`).parentElement;
-        let toDoListElement = document.querySelector('#toDoList');
-        toDoListElement.append(elem);
+        // taskElement.checked = false;
+
 
         // if (canChecked) {
         //
@@ -64,7 +78,8 @@ const changeTaskStatus = (taskElement) => {
 
 const createTask = (id, text) => {
     const taskElement = document.createElement("div");
-    taskElement.setAttribute('id', `${'div_'+id}`)
+    taskElement.setAttribute('id', `${'div_'+id}`);
+    taskElement.setAttribute('taskstatus', `${false}`);
     taskElement.innerHTML = `<input type="checkbox" id="${id}" name="scales"><label for="${id}">${text}</label><button>DELETE</button>`;
     const checkBox = taskElement.firstChild;
     const deleteButton = taskElement.lastChild;
